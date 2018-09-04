c21aab4e287bbe287784c0e528135c768cb69467
Peter Kraml <piets@macpiets.net>
2018-09-04 14:33:41 +0200

    clean up code to make it similiar to smartwares-rfsend

--- unitec-rfsend.c
+++ unitec-rfsend.c
@@ -7,46 +7,55 @@ int shortPulse = 450;
 int longPulse = 950;
 
 void sendBit(int bit, int pin) {
-  if (bit == 0) {
-    digitalWrite(pin, HIGH);
-    delayMicroseconds(longPulse);
-    digitalWrite(pin, LOW);
-    delayMicroseconds(shortPulse);
-  } else if (bit == 1) {
-    digitalWrite(pin, HIGH);
-    delayMicroseconds(shortPulse);
-    digitalWrite(pin, LOW);
-    delayMicroseconds(longPulse);
-  }
+    if (bit == 0) {
+        digitalWrite(pin, HIGH);
+        delayMicroseconds(longPulse);
+        digitalWrite(pin, LOW);
+        delayMicroseconds(shortPulse);
+    } else if (bit == 1) {
+        digitalWrite(pin, HIGH);
+        delayMicroseconds(shortPulse);
+        digitalWrite(pin, LOW);
+        delayMicroseconds(longPulse);
+    }
 }
 
-void pulse(char codeString[], int pin) {
-  int i, h;
+void sendCommand(char codeString[], int pin) {
+    int i, h;
 
-  for (h = 0; h < 5; h++) {
-    digitalWrite(pin, HIGH);
-    delayMicroseconds(shortPulse);
-    digitalWrite(pin, LOW);
-    delayMicroseconds(5 * shortPulse);
+    for (h = 0; h < 5; h++) {
+        // Send Header
+        digitalWrite(pin, HIGH);
+        delayMicroseconds(shortPulse);
+        digitalWrite(pin, LOW);
+        delayMicroseconds(5 * shortPulse);
 
-    for (i = 0; i < strlen(codeString); i++){
-      sendBit(codeString[i] - '0', pin);
+        // Send Command
+        for (i = 0; i < strlen(codeString); i++){
+            sendBit(codeString[i] - '0', pin);
+        }
     }
-  }
 }
 
 int main (int argc, char **argv) {
-  wiringPiSetup();
+    if (wiringPiSetup() == -1) {
+        return -1;
+    }
+    piHiPri(99);
 
-  int pin = atoi(argv[2]);
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, LOW);
+    // Load Parameters
+    // <COMMAND> <PIN>
+    char *codeString = argv[1];
+    int pin = atoi(argv[2]);
 
-  int h;
-  for (h = 0; h < 3; h++) {
-    pulse(argv[1], pin);
-    delayMicroseconds(5 * longPulse);
-  }
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
 
-  return 0;
+    int i;
+    for (i = 0; i < 4; i++) {
+        sendCommand(codeString, pin);
+        delay(10);
+    }
+
+    return 0;
 }
